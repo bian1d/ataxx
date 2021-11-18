5c71106b771378d9bc3b0f555b713290a83d72fe
bian1d <91188079+bian1d@users.noreply.github.com>
2021-11-18 19:39:32 +0800

    Add files via upload
    这是一个模板，省得你天天include一堆东西

---
+++ template file.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>//memset,strlen
+#include<string>//string类型
+#include<algorithm>//sort
+#include<iomanip>//setfill(' ')<<setw(4)<<
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+/*
+for (int =;<=;++)
+{}
+memset(,0,sizeof());
+
+*/
